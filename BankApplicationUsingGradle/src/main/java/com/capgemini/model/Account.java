c37570030e0d2d0419524cd1a3b73297cf906183
Wanjari <chetan.wanjari@capgemini.com>
2017-06-21 13:15:20 +0530

    Bank App

---
+++ BankApplicationUsingGradle/src/main/java/com/capgemini/model/Account.java
@@ -0,0 +1,38 @@
+package com.capgemini.model;
+
+public class Account implements Cloneable {
+	
+	private int accountNumber;
+	private int amount;
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (accountNumber != other.accountNumber)
+			return false;
+		if (amount != other.amount)
+			return false;
+		return true;
+	}
+	
+	
+
+}
